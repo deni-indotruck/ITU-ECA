0e45aece2e53d127b290aa665c05014e16deefea
deni-indotruck <ahmad.wahyudi@indotruck-utama.co.id>
2023-02-24 15:59:33 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to ITU-ECA" });
+});
+
+const privateKey = fs.readFileSync(
+  path.join(__dirname, "/keys/private.key"),
+  "utf8"
+);
+const bundle = fs.readFileSync(
+  path.join(__dirname, "/keys/bundle.ca-bundle"),
+  "utf8"
+);
+
+const certificate = fs.readFileSync(
+  path.join(__dirname, "/keys/certificate.crt"),
+  "utf8"
+);
+
+https
+  .createServer(
+    {
+      key: privateKey,
+      cert: certificate,
+    },
+    app
+  )
+  .listen(5050, () =>
+    console.log("HTTPS Server are created and started at port 5001")
+  );
+
+app.listen(process.env.PORT || 5051, function () {
+  console.info("HTTP Server Started at port", process.env.PORT || 5051);
+});
