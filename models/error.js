e50923ac8353a6fb4dbb266517796ab58af4fb3f
deni-indotruck <ahmad.wahyudi@indotruck-utama.co.id>
2023-02-24 17:06:48 +0700

    Update error.js

--- models/error.js
+++ models/error.js
@@ -1,52 +1,50 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-module.exports = (mongoose) => {
-  var Error = mongoose.Schema(
-    {
-      company_code: {
-        type: String,
-
-        default: "",
-      },
-      machine: {
-        type: String,
-        default: "",
-      },
-      date: {
-        type: Date,
-        default: "",
-      },
-      error_type: {
-        type: String,
-        default: "",
-      },
-      priority: {
-        type: String,
-        default: "",
-      },
-      counter: {
-        type: String,
-        default: "",
-      },
-      company: {
-        type: String,
-        default: "",
-      },
-      model: {
-        type: String,
-        default: "",
-      },
-      serial_number: {
-        type: String,
-        default: "",
-      },
-      local_code: {
-        type: String,
-        default: "",
-      },
+const Error = new Schema(
+  {
+    company_code: {
+      type: String,
+      default: "",
     },
-    { timestamps: true }
-  );
-  const error = mongoose.model("error", Error);
-  return error;
-};
+    machine: {
+      type: String,
+      default: "",
+    },
+    date: {
+      type: Date,
+      default: "",
+    },
+    error_type: {
+      type: String,
+      default: "",
+    },
+    priority: {
+      type: String,
+      default: "",
+    },
+    counter: {
+      type: String,
+      default: "",
+    },
+    company: {
+      type: String,
+      default: "",
+    },
+    model: {
+      type: String,
+      default: "",
+    },
+    serial_number: {
+      type: String,
+      default: "",
+    },
+    local_code: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("error", Error);
