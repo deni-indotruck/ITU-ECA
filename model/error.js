c7580cc920d72ad9c1300427e4a373f6a9422f57
rizaldi-itu <rizaldi.akbar@indotruck-utama.co.id>
2023-02-24 17:03:13 +0700

    Update error.js

--- model/error.js
+++ model/error.js
@@ -1,75 +1,50 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-module.exports = (mongoose) => {
-  var error = mongoose.Schema(
-    {
-      company_code: {
-        type: String,
-        default: "",
-      },
-      machine: {
-        type: String,
-        default: "",
-      },
-      date: {
-        type: Date,
-        default: "",
-      },
-      error_type: {
-        type: String,
-        default: "",
-      },
-      priority: {
-        type: String,
-        default: "",
-      },
-      counter: {
-        type: String,
-        default: "",
-      },
-      company: {
-        type: String,
-        default: "",
-      },
-      model: {
-        type: String,
-        default: "",
-      },
-      serial_number: {
-        type: String,
-        default: "",
-      },
-      local_code: {
-        type: String,
-        default: "",
-      },
-
-      // username: {
-      //   type: String,
-      // },
-      // role: {
-      //   type: mongoose.Schema.Types.ObjectId,
-      //   ref: "Role",
-      // },
-      // books: [
-      //   {
-      //     type: mongoose.Schema.Types.ObjectId,
-      //     ref: "Book",
-      //     // required: true,
-      //     // default: "",
-      //   },
-      // ],
+const Error = new Schema(
+  {
+    company_code: {
+      type: String,
+      default: "",
     },
-    { timestamps: true }
-  );
+    machine: {
+      type: String,
+      default: "",
+    },
+    date: {
+      type: Date,
+      default: "",
+    },
+    error_type: {
+      type: String,
+      default: "",
+    },
+    priority: {
+      type: String,
+      default: "",
+    },
+    counter: {
+      type: String,
+      default: "",
+    },
+    company: {
+      type: String,
+      default: "",
+    },
+    model: {
+      type: String,
+      default: "",
+    },
+    serial_number: {
+      type: String,
+      default: "",
+    },
+    local_code: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
 
-  // user.method("toJSON", function () {
-  //   const { __v, _id, ...object } = this.toObject();
-  //   object.id = _id;
-  //   return object;
-  // });
-  // error.plugin(mongoosePaginate);
-
-  const Error = mongoose.model("Error", error);
-  return Error;
-};
+module.exports = mongoose.model("error", Error);
