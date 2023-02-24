2a9136c6992092744b53fe2c4bcfb77570fc22ff
rizaldi-itu <rizaldi.akbar@indotruck-utama.co.id>
2023-02-24 16:56:25 +0700

    Update error.js

--- model/error.js
+++ model/error.js
@@ -5,33 +5,43 @@ module.exports = (mongoose) => {
     {
       company_code: {
         type: String,
+        default: "",
       },
       machine: {
         type: String,
+        default: "",
       },
       date: {
         type: Date,
+        default: "",
       },
       error_type: {
         type: String,
+        default: "",
       },
       priority: {
         type: String,
+        default: "",
       },
       counter: {
         type: String,
+        default: "",
       },
       company: {
         type: String,
+        default: "",
       },
       model: {
         type: String,
+        default: "",
       },
       serial_number: {
         type: String,
+        default: "",
       },
       local_code: {
         type: String,
+        default: "",
       },
 
       // username: {
