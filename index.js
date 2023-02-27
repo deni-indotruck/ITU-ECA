6aed89e42eb488eefbfe9357dc33525afdd8b39f
deni-indotruck <ahmad.wahyudi@indotruck-utama.co.id>
2023-02-27 13:40:06 +0700

    update server status

--- index.js
+++ index.js
@@ -2,25 +2,31 @@ const express = require("express");
 const https = require("https");
 const fs = require("fs");
 const path = require("path");
+const actuator = require("express-actuator");
 require("dotenv").config();
 require("./config/db");
 const importAlert = require("./config/importAlert");
 const importError = require("./config/importError");
-
 const importDatatable = require("./config/importDataTables");
 const route = require("./routes/index");
+const options = {
+  basePath: "/api", // It will set /management/info instead of /info
+  infoGitMode: "full", // the amount of git information you want to expose, 'simple' or 'full',
+  infoBuildOptions: null, // extra information you want to expose in the build object. Requires an object.
+  infoDateFormat: null, // by default, git.commit.time will show as is defined in git.properties. If infoDateFormat is defined, moment will format git.commit.time. See https://momentjs.com/docs/#/displaying/format/.
+  customEndpoints: [], // array of custom endpoints
+};
 
 const app = express();
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
+app.use(actuator(options));
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to ITU-ECA" });
 });
 
 app.use([importAlert, importError, importDatatable, route]);
-
 
 const privateKey = fs.readFileSync(
   path.join(__dirname, "/keys/private.key"),
